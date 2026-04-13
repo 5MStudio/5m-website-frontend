095ecf492c0bbcadd37119743b70c43737aab57f
eonsoftime <moritz@buesselberg.net>
2026-04-13 23:25:13 +0200

    update

--- types/about.ts
+++ types/about.ts
@@ -1,9 +1,19 @@
+export interface ContactItem {
+  label: string
+  email: string
+}
+
+export interface PlatformItem {
+  label: string
+  url: string
+}
+
 export interface About {
   _id: string
   studioText?: any[]
   services?: string[]
   clients?: string[]
   offices?: string[]
-  contact?: any[]
-  platforms?: string[]
+  contact?: ContactItem[]
+  platforms?: PlatformItem[]
 }
